e370661cf69fda37746538b58e6541bbf79c6f61
Michael Mai <mm@mai-internet.de>
2019-07-08 08:09:03 +0200

    set copyright

---
+++ includes/copyright.php
@@ -0,0 +1,5 @@
+<div class="fl-page-footer-text fl-page-footer-text-1">
+	<span>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?></span>
+	<span> | </span>
+    Kompetenzzentrum Demenz in Schleswig-Holstein - Ein Projekt der Alzheimer Gesellschaft Schleswig-Holstein e.V. Selbsthilfe Demenz / Landesverband
+</div>
