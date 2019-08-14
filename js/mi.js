f3e0064a1131b859c54b14172f7a5e81519e4f4f
Michael Mai <mm@mai-internet.de>
2019-08-14 14:46:23 +0200

    interaktive Karte mspeth

--- js/mi.js
+++ js/mi.js
@@ -2,5 +2,13 @@
  * Created by mmai on the birthday of the flying spagetthi monster
  */
 jQuery(document).ready(function($) {
-
+    alert('hi');
+    function mapHighlight(key){
+        document.getElementById('shmap').classList.add(key);
+        document.getElementById('legende_'+key).classList.add('highlight');
+    }
+    function mapReset(key){
+        document.getElementById('shmap').classList.remove(key);
+        document.getElementById('legende_'+key).classList.remove('highlight');
+    }
 });
