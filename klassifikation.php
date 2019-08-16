847f11241fd6761d33ed0fbec1dd6645cc6a02b2
Michael Mai <mm@mai-internet.de>
2019-08-16 07:25:44 +0200

    templates for listing klassifikationen

---
+++ klassifikation.php
@@ -0,0 +1,31 @@
+<?php
+
+$show_thumbs = FLTheme::get_setting( 'fl-archive-show-thumbs' );
+$show_full   = apply_filters( 'fl_archive_show_full', FLTheme::get_setting( 'fl-archive-show-full' ) );
+$more_text   = FLTheme::get_setting( 'fl-archive-readmore-text' );
+$thumb_size  = FLTheme::get_setting( 'fl-archive-thumb-size', 'large' );
+
+do_action( 'fl_before_post' ); ?>
+<div class="m-4">
+    <h4><?php the_title(); ?></h4>
+	<?php if ( get_field( 'untertitel' ) ): ?>
+        <h5 class="card-subtitle mb-2 text-muted"><?php the_field( 'untertitel' ); ?></h5>
+	<?php endif; ?>
+    <div>
+		<?php if ( get_field( 'strasse' ) ): ?>
+            <address>
+				<?php the_field( 'strasse' ); ?><br>
+				<?php the_field( 'plz' ); ?>&nbsp;<?php the_field( 'ort' ); ?>
+            </address>
+		<?php endif; ?>
+		<?php if ( get_field( 'telefon' ) ): ?>
+            Tel.: <?php the_field( 'telefon' ); ?><br>
+		<?php endif; ?>
+		<?php if ( get_field( 'website' ) ): ?>
+            Website: <?php do_action( 'format_website', 'website' ); ?><br>
+		<?php endif; ?>
+		<?php the_content(); ?>
+    </div>
+    <a href="#" class="card-link">Kreis Dithmarschen</a>
+    <a href="#" class="card-link"></a>
+</div>
---
+++ taxonomy-klassifikation.php
@@ -0,0 +1,44 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Silence is golden.
+}
+?>
+<?php get_header(); ?>
+
+    <div class="fl-archive <?php FLLayout::container_class(); ?>">
+        <div class="">
+
+            <div class="fl-content"<?php FLTheme::print_schema( ' itemscope="itemscope" itemtype="https://schema.org/Blog"' ); ?>>
+
+                <h2><?= single_term_title() ?></h2>
+				<?php FLTheme::archive_page_header(); ?>
+
+				<?php if ( have_posts() ) : ?>
+
+
+						<?php
+						while ( have_posts() ) :
+							the_post();
+							?>
+							<?php
+							// $post_format = get_post_format();
+							get_template_part( 'klassifikation', '' );
+							?>
+						<?php endwhile; ?>
+
+
+
+					<?php FLTheme::archive_nav(); ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'content', 'no-results' ); ?>
+
+				<?php endif; ?>
+
+            </div>
+
+        </div>
+    </div>
+
+<?php get_footer(); ?>
