b548d426d1f7ffcf2e1fde6307a5179add966795
Michael Mai <mm@mai-internet.de>
2019-08-19 11:02:32 +0200

    style and custom ui for homepage

--- templates/searchhome.php
+++ templates/searchhome.php
@@ -45,7 +45,22 @@ $taxonomies = get_terms( [
         </div>
         <!--   Tab 2 -->
         <div class="tab-pane col align-self-center fade" id="volltext" role="tabpanel" aria-labelledby="volltext-tab">
-
+            <div class="row">
+                <div class="col-md-12">
+                    <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo( 'url' ) ?>">
+                        <div class="input-group mb-3">
+                            <label class="screen-reader-text" for="s">Volltextsuche nach:</label>
+                            <input type="text" name="s" id="s" class="form-control" placeholder="Volltextsuche" aria-label="volltext"
+                                   aria-describedby="plz">
+                            <div class="input-group-append fl-button-wrap fl-button-width-auto fl-button-left fl-button-has-icon">
+                                <button type="submit" class="fl-button" role="button">
+                                    <span class="fl-button-text">Suchen</span>
+                                    <i class="fl-button-icon fl-button-icon-after ua-icon ua-icon-Search" aria-hidden="true"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-</div>
