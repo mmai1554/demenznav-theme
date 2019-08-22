30d3aec54faed4a7dfcd886677a28149814a029f
Michael Mai <mm@mai-internet.de>
2019-08-22 10:52:30 +0200

    implement klassifikationen Liste

--- klassifikation.php
+++ klassifikation.php
@@ -6,28 +6,16 @@ $more_text   = FLTheme::get_setting( 'fl-archive-readmore-text' );
 $thumb_size  = FLTheme::get_setting( 'fl-archive-thumb-size', 'large' );
 
 $klassifikationen = $categories = get_the_terms( $post, 'klassifikation' );
-$kreise = $categories = get_the_terms( $post, 'kreis' );
+$kreise           = $categories = get_the_terms( $post, 'kreis' );
 do_action( 'fl_before_post' ); ?>
-<div class="m-4">
+<div class="tax-klassifikation">
     <h4><?php the_title(); ?></h4>
 	<?php if ( get_field( 'untertitel' ) ): ?>
-        <h5 class="card-subtitle mb-2 text-muted"><?php the_field( 'untertitel' ); ?></h5>
+        <p class="mb-2 text-muted"><?php the_field( 'untertitel' ); ?></p>
 	<?php endif; ?>
-    <div>
-		<?php if ( get_field( 'strasse' ) ): ?>
-            <address>
-				<?php the_field( 'strasse' ); ?><br>
-				<?php the_field( 'plz' ); ?>&nbsp;<?php the_field( 'ort' ); ?>
-            </address>
-		<?php endif; ?>
-		<?php if ( get_field( 'telefon' ) ): ?>
-            Tel.: <?php the_field( 'telefon' ); ?><br>
-		<?php endif; ?>
-		<?php if ( get_field( 'website' ) ): ?>
-            Website: <?php do_action( 'format_website', 'website' ); ?><br>
-		<?php endif; ?>
-		<?php the_content(); ?>
+	<?php the_content(); ?>
+    <div class="mi-list-icons">
+        <?php do_action( 'format_contact' ); ?>
     </div>
-    <a href="#" class="card-link">Kreis Dithmarschen</a>
-    <a href="#" class="card-link"></a>
+    <hr>
 </div>
