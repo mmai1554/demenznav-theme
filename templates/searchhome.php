f0f6f476d07659562691368430a3d6310146bd46
Michael Mai <mm@mai-internet.de>
2019-10-04 06:53:00 +0200

    add xajax handler to process results via xajax

--- templates/searchhome.php
+++ templates/searchhome.php
@@ -47,26 +47,29 @@ $lk         = [
     <div class="tab-content h-100 row rounded" id="myTabContent">
         <!-- Tab 1 -->
         <div class="tab-pane col align-self-center fade show active" id="radius" role="tabpanel" aria-labelledby="radius-tab">
-            <div class="row">
-                <div class="col-md-6 pr-1">
-                    <select class="custom-select" id="Einrichtung">
-                        <option value="">Hilfsangebot wählen</option>
-						<?php foreach ( $taxonomies as $tax ) : ?>
-                            <option value="K<?= $tax->term_id ?>"><?= $tax->name ?></option>
-						<?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="col-md-6 pl-1">
-                    <div class="input-group mb-3">
-                        <label for="plz" class="px-2">Wo:</label>
-                        <input type="text" class="form-control" id="plz" placeholder="Bitte Postleitzahl eingeben" aria-label="PLZ"
-                               aria-describedby="plz">
-                        <div class="input-group-append">
-                            <button class="" type="button">Suche starten</button>
+<!--            <form class="mnc-ajaxform" action="--><?//= get_page_link( 721 ); // 721 = Umkreissuche  ?><!--" method="get">-->
+            <form id="FormUmkreissuche" action="<?= get_page_link( 721 ); // 721 = Umkreissuche  ?>" method="get">
+                <div class="row">
+                    <div class="col-md-6 pr-1">
+                        <select name="mnc[einrichtung]" class="custom-select" id="Einrichtung">
+                            <option value="">Hilfsangebot wählen</option>
+							<?php foreach ( $taxonomies as $tax ) : ?>
+                                <option value="K<?= $tax->term_id ?>"><?= $tax->name ?></option>
+							<?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="col-md-6 pl-1">
+                        <div class="input-group mb-3">
+                            <label for="plz" class="px-2">Wo:</label>
+                            <input name="mnc[plz]" type="text" class="form-control" id="plz" placeholder="Bitte Postleitzahl eingeben" aria-label="PLZ"
+                                   aria-describedby="plz">
+                            <div class="input-group-append">
+                                <input type="submit" value="Suche starten">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
         <!--   Tab 2 -->
         <div class="tab-pane col align-self-center fade" id="volltext" role="tabpanel" aria-labelledby="volltext-tab">
