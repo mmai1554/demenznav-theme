b1c18e9677dd2b04695cedb9893fc426b4bc5df3
Michael Mai <mm@mai-internet.de>
2019-10-09 18:35:28 +0200

    implement contact in template instead of hook action

--- templates/einrichtung.php
+++ templates/einrichtung.php
@@ -1,5 +1,7 @@
 <?php
 
+use mnc\Maln;
+
 $show_thumbs = FLTheme::get_setting( 'fl-archive-show-thumbs' );
 $show_full   = apply_filters( 'fl_archive_show_full', FLTheme::get_setting( 'fl-archive-show-full' ) );
 $more_text   = FLTheme::get_setting( 'fl-archive-readmore-text' );
@@ -14,13 +16,41 @@ do_action( 'fl_before_post' ); ?>
         <p class="mb-2 text-muted"><?php the_field( 'untertitel' ); ?></p>
 	<?php endif; ?>
     <div class="badges">
-        <?php foreach($kreise as $kreis): ?>
-            <a href="<?php echo(get_category_link( $kreis )); ?>"><span class="badge badge-secondary"><?php echo( $kreis->name); ?></span></a>&nbsp;
-        <?php endforeach; ?>
+		<?php foreach ( $kreise as $kreis ): ?>
+            <a href="<?php echo( get_category_link( $kreis ) ); ?>"><span class="badge badge-secondary"><?php echo( $kreis->name ); ?></span></a>&nbsp;
+		<?php endforeach; ?>
     </div>
 	<?php the_content(); ?>
     <div class="mi-list-icons">
-        <?php do_action( 'format_contact' ); ?>
+		<?php
+		$contact = [];
+		if ( $url = get_field( 'website', false, false ) ) {
+			$content            = sprintf( '<a href="%s" target="_blank" title="Website %s in neuem Fenster öffnen...">%s</a>', $url, $url, $url );
+			$contact['website'] = [ 'Web: ', $content, 'ua-icon-globe2' ];
+		}
+		if ( $email = get_field( 'email', false, false ) ) {
+			$content          = sprintf( '<a href="%s" target="_blank" title="Mailprogramm öffnen und E-Mail an %s senden...">%s</a>', $email, $email, $email );
+			$contact['email'] = [ 'E-Mail: ', $content, 'ua-icon-icon-6-mail-envelope-closed2' ];
+		}
+		if ( get_field( 'strasse' ) ) {
+			$content            = get_field( 'strasse' ) . '<br>' . get_field( 'plz' ) . ' ' . get_field( 'ort' );
+			$contact['adresse'] = [ '', $content, 'ua-icon-location-pin' ];
+		}
+		if ( get_field( 'telefon' ) ) {
+			$content            = get_field( 'telefon' );
+			$contact['telefon'] = [ 'Telefon: ', $content, 'ua-icon-phone3' ];
+		}
+		if ( count( $contact ) == 0 ) {
+			return '';
+		}
+		$html = '<ul>';
+		foreach ( $contact as $key => $line ) {
+			$html .= Maln::icon_li( $line[0] . $line[1], $line[2] );
+		}
+		$html .= '</ul>';
+		echo( $html );
+
+		?>
     </div>
     <hr>
 </div>
