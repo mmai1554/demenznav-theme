95fa40bd214a5aeafb65d82b6f4e60a5d770b142
Michael Mai <mm@mai-internet.de>
2019-10-09 17:25:22 +0200

    refactor. outsource forms for multiple use

--- templates/form_landkreisekarte.php
+++ templates/form_landkreisekarte.php
@@ -1,40 +1,57 @@
 <?php
-if ( ! isset( $taxonomies ) ) {
-	$taxonomies = get_terms( [
-		'taxonomy'   => 'klassifikation',
-		'hide_empty' => false,
-		'parent'     => 0
-	] );
+if ( ! isset( $lk ) ) {
+	$lk = [
+		'<a href="https://demenzwegweiser-sh.de/kreis/dithmarschen/">Dithmarschen</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/herzogtum-lauenburg/">Herzogtum Lauenburg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/nordfriesland/">Nordfriesland</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/ostholstein/">Ostholstein</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/pinneberg/">Pinneberg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/ploen/">Plön</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/rendsburg-eckernfoerde/">Rendsburg-Eckernförde</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/schleswig-flensburg/">Schleswig-Flensburg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/segeberg/">Segeberg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/steinburg/">Steinburg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/stormarn/">Stormarn</a>',
+		// index Flensburg = 11
+		'<a href="https://demenzwegweiser-sh.de/kreis/flensburg/">Flensburg</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/kiel/">Kiel</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/luebeck/">Lübeck</a>',
+		'<a href="https://demenzwegweiser-sh.de/kreis/neumuenster/">Neumünster</a>',
+	];
 }
 ?>
-<form action="<?= get_page_link( 721 ); // 721 = Umkreissuche      ?>" method="get">
+
+<div class="col-md-4 py-3">
+    <img src="<?= site_url() . '/wp-content/uploads/2019/08/SH_blau.svg' ?>">
+</div>
+<div class="col-md-8 list-kreise">
     <div class="row">
-        <div class="col-md-7 pr-1">
-            <div class="input-group">
-                <select name="mnc-einrichtung" class="custom-select" id="Einrichtung" style="width:65%;">
-                    <option value="">Hilfsangebot wählen</option>
-					<?php foreach ( $taxonomies as $tax ) : ?>
-                        <option value="K<?= $tax->term_id ?>"><?= $tax->name ?></option>
-					<?php endforeach; ?>
-                </select>
-                <select name="mnc-rmax" class="custom-select" id="MncRmax">
-                    <option value="">Umkreis</option>
-                    <option value="10">10 km</option>
-                    <option value="25">25 km</option>
-                    <option value="50">50 km</option>
-                    <option value="100">100 km</option>
-                </select>
-            </div>
+        <div class="col">
+            <ul class="arrowlink">
+                <li><?= $lk[0] ?></li>
+                <li><?= $lk[1] ?></li>
+                <li><?= $lk[2] ?></li>
+                <li><?= $lk[3] ?></li>
+                <li><?= $lk[4] ?></li>
+                <li><?= $lk[5] ?></li>
+            </ul>
         </div>
-        <div class="col-md-5 pl-1">
-            <div class="input-group mb-3">
-                <label for="mnc-plz" class="px-2">von:</label>
-                <input name="mnc-plz" type="text" class="form-control" id="plz" placeholder="Bitte Postleitzahl eingeben" aria-label="PLZ"
-                       aria-describedby="plz">
-                <div class="input-group-append">
-                    <input type="submit" value="Suche starten">
-                </div>
-            </div>
+        <div class="col">
+            <ul class="arrowlink">
+                <li><?= $lk[6] ?></li>
+                <li><?= $lk[7] ?></li>
+                <li><?= $lk[8] ?></li>
+                <li><?= $lk[9] ?></li>
+                <li><?= $lk[10] ?></li>
+            </ul>
+        </div>
+        <div class="col">
+            <ul class="arrowlink">
+                <li><?= $lk[11] ?></li>
+                <li><?= $lk[12] ?></li>
+                <li><?= $lk[13] ?></li>
+                <li><?= $lk[14] ?></li>
+            </ul>
         </div>
     </div>
-</form>
+</div>
