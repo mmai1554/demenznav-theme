e0a673f4b131924a8848b344ec7a35bc4da1c67a
Michael Mai <mm@mai-internet.de>
2019-11-04 11:56:26 +0100

    set height for gmaps container, remove unused script

--- js/mi.js
+++ js/mi.js
@@ -2,13 +2,5 @@
  * Created by mmai on the birthday of the flying spagetthi monster
  */
 jQuery(document).ready(function($) {
-    alert('hi');
-    function mapHighlight(key){
-        document.getElementById('shmap').classList.add(key);
-        document.getElementById('legende_'+key).classList.add('highlight');
-    }
-    function mapReset(key){
-        document.getElementById('shmap').classList.remove(key);
-        document.getElementById('legende_'+key).classList.remove('highlight');
-    }
+
 });
