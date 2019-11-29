5bb4d489e233a2ed353cbff690ad011c24a42b8d
Michael Mai <mm@mai-internet.de>
2019-11-29 15:35:10 +0100

    activate under construction

--- templates/under_construction.php
+++ templates/under_construction.php
@@ -1,13 +1,14 @@
 <div class="row">
     <div class="col-md-12">
-        <p>Die Suche auf dieser Website wird ab dem 01.03.2020 nutzbar sein. Bis dahin stehen Ihnen aber die folgenden Seiten zur Suche von Hilfsangeboten zur
-            Verfügung:</p>
+        <div class="p-4">
+            <p>Die Suche auf dieser Website wird ab dem 01.03.2020 nutzbar sein. Bis dahin stehen Ihnen aber die folgenden Seiten zur Suche von Hilfsangeboten zur
+                Verfügung:</p>
 
-        <ul>
-            <li><a href="https://www.pflege-navigator.de/" target="_blank">Pflegenavigator der AOK</a></li>
-            <li><a href="https://www.bkk-pflegefinder.de/" target="_blank">BKK Pflegefinder</a></li>
-            <li><a href="https://www.pflegelotse.de/presentation/pl_startseite.aspx" target="_blank">Pflegelotse vdek</a></li>
-        </ul>
-
+            <ul>
+                <li><a href="https://www.pflege-navigator.de/" target="_blank">Pflegenavigator der AOK</a></li>
+                <li><a href="https://www.bkk-pflegefinder.de/" target="_blank">BKK Pflegefinder</a></li>
+                <li><a href="https://www.pflegelotse.de/presentation/pl_startseite.aspx" target="_blank">Pflegelotse vdek</a></li>
+            </ul>
+        </div>
     </div>
 </div>
