7942241c0977551c36767b8637ee61bc522a0f36
Michael Mai <mm@mai-internet.de>
2019-11-29 15:22:01 +0100

    add template no search available

---
+++ templates/under_construction.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-12">
+        <p>Die Suche auf dieser Website wird ab dem 01.03.2020 nutzbar sein. Bis dahin stehen Ihnen aber die folgenden Seiten zur Suche von Hilfsangeboten zur
+            Verfügung:</p>
+
+        <ul>
+            <li><a href="https://www.pflege-navigator.de/" target="_blank">Pflegenavigator der AOK</a></li>
+            <li><a href="https://www.bkk-pflegefinder.de/" target="_blank">BKK Pflegefinder</a></li>
+            <li><a href="https://www.pflegelotse.de/presentation/pl_startseite.aspx" target="_blank">Pflegelotse vdek</a></li>
+        </ul>
+
+    </div>
+</div>
