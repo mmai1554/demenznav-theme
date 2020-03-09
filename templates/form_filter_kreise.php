1c0e160592c0b453a5a296913ee0c47abb8adb67
Michael Mai <mm@mai-internet.de>
2020-03-09 22:06:25 +0100

    move setting variable mnc-einrichtung out of foreach loop

--- templates/form_filter_kreise.php
+++ templates/form_filter_kreise.php
@@ -15,7 +15,7 @@ if ( ! isset( $taxonomies ) ) {
 }
 
 $seletced_plz = isset( $_GET[ Umkreissuche::QUERY_VAR_PLZ ] ) ? 'value="' . $_GET[ Umkreissuche::QUERY_VAR_PLZ ] . '"' : '';
-
+$mnc_einrichtung =  $_GET[ Umkreissuche::QUERY_VAR_KLASSIFIKATION ]  ?? '';
 ?>
 <div class="searchhome">
     <form method="get">
@@ -25,7 +25,6 @@ $seletced_plz = isset( $_GET[ Umkreissuche::QUERY_VAR_PLZ ] ) ? 'value="' . $_GE
                     <select name="mnc-einrichtung" class="custom-select" id="Einrichtung">
                         <option value="">Alle Einrichtungen</option>
 						<?php foreach ( $taxonomies as $tax ) :
-							$mnc_einrichtung =  $_GET[ Umkreissuche::QUERY_VAR_KLASSIFIKATION ]  ?? '';
 							$selected = $mnc_einrichtung == 'K' . $tax->term_id ? ' selected' : '';
 							?>
                             <option value="K<?= $tax->term_id ?>" <?= $selected ?>><?= $tax->name ?></option>
